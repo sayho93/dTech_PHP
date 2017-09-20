3cf5d78862777829390d5e88963be977c8f07079
sayhoChun <fishcreek@naver.com>
2017-09-20 18:55:38 +0900

    hotfix) trendView mobile publishing addedf

---
+++ wapp/mobile/Step1/trendView.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sayho
+ * Date: 2017-09-20
+ * Time: 오후 6:54
+ */
+?>
+
+
+<div class="spectrum_view step2_view">
+    <div class="view_title">
+        <p>Trend View</p>
+        <a href="#" class="exit_btn"><img src="image/ic_pop_exit.png" alt="닫기" /></a>
+    </div>
+
+    <div class="form_select clearfix">
+        <select class="f_l">
+            <option>모터고정자 결함</option>
+        </select>
+
+        <div class="date_select f_r">
+            <span>트렌드 기간</span>
+            <input type="button" name="" value="2017-01-01" />
+            <span>~</span>
+            <input type="button" name="" value="2017-01-15" />
+        </div>
+    </div>
+
+    <div class="graph_area02">
+        <div class="graph"><!-- 그래프 들어가는 곳 --></div>
+    </div>
+
+    <div class="view_title" style="margin-top:4vh;">
+        <p>Spectrum View</p>
+    </div>
+
+    <div class="graph_area02" style="margin:2.77vh 0;">
+        <div class="graph"><!-- 그래프 들어가는 곳 --></div>
+    </div>
+</div>
